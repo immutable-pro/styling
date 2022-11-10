41c015680127f0fc2f433aaf4f0787e5e2ccd108
Jorge Garcia Rodriguez <jgarcia@rapidapi.com>
2022-11-10 12:00:33 +0000

    chore: using raw CSS in React

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,5 +1,7 @@
+import './style.css'
+
 interface ButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {}
 
 export const Button = (props: ButtonProps) => {
-   return (<button {...props}/>)
+   return (<button className="button-exp" {...props}/>)
 }

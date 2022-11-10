6c030f65e6e7dcf5df89d368a8be544b395a9c7f
Jorge Garcia Rodriguez <jgarcia@rapidapi.com>
2022-11-10 12:05:49 +0000

    chore: using raw CSS in React with CLSX

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,7 +1,8 @@
+import clsx from 'clsx'
 import './style.css'
 
 interface ButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {}
 
 export const Button = (props: ButtonProps) => {
-   return (<button className="button-exp" {...props}/>)
+   return (<button className={clsx("button-exp")} {...props}/>)
 }
